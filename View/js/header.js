e49f8a88ba74fdd4e4e4ed2b27c748e5a41af10a
Nickolas Davi <105878022+niickol4s@users.noreply.github.com>
2024-08-28 18:02:55 -0300

    Add files via upload

--- View/js/header.js
+++ View/js/header.js
@@ -4,9 +4,9 @@ class Header extends HTMLElement {
             <header>
                 <h2>Biblioteca Online</h2>
                 <nav>
-                    <a href="./livro/pagina_livro.html">Livros</a>
-                    <a href="./autor/pagina_autor.html">Autores</a>
-                    <a href="#estudantes">Estudantes</a>
+                    <a href="./livro/pagina_livro.php">Livros</a>
+                    <a href="./autor/pagina_autor.php">Autores</a>
+                    <a href="./estudante/pagina_estudante.php">Estudantes</a>
                     <a href="#emprestimos">Empréstimos</a>
                 </nav>
             </header>
